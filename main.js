84cae91a64fa7589c4d21f65b9108428142b5de7
1234mukihudsa <94767139+1234mukihudsa@users.noreply.github.com>
2021-12-21 08:17:19 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function fire()
+{
+    window.location="fire.html";
+}
+function back(){
+    window.location="index.html";
+}
+function police(){
+    window.location="police.html";
+}
+function ambulance(){
+    window.location="ambulance.html";
+}
